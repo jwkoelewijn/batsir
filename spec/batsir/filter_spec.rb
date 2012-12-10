7d035feacf84df1e0959f9ac796fced250d34707
Bram de Vries <dev@bramdevries.net>
2012-12-10 21:03:30 +0100

    Replaced checking for a sane Ruby environment with something useful

--- spec/batsir/filter_spec.rb
+++ spec/batsir/filter_spec.rb
@@ -1,12 +1,11 @@
 require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
 describe Batsir::Filter do
-  it "can create an filter" do
-    filter = Batsir::Filter.new
-    filter.should_not be_nil
-  end
-
   it "has an #execute method" do
     Batsir::Filter.instance_methods.map{|im| im.to_s}.should include "execute"
+  end
+
+  it "throws an NotImplementedError when #execute method is not overridden" do
+    lambda{subject.execute("testing..1..2..3")}.should raise_error NotImplementedError
   end
 end
