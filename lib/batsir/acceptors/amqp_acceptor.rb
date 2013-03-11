2e0100162f34aca7a276ce199d1e597518d8672b
Bram de Vries <dev@bramdevries.net>
2013-03-11 14:28:49 +0100

    Make sure a queue is bound to an exchange

--- lib/batsir/acceptors/amqp_acceptor.rb
+++ lib/batsir/acceptors/amqp_acceptor.rb
@@ -12,7 +12,7 @@ module Batsir
         bunny_pool.with do |bunny|
           q = bunny.queue( queue )
           x = bunny.exchange( exchange )
-          q.bind( x, :routing_key => queue)
+          bind_queue_to_exchange(q,x)
 
           @consumer = Batsir::AMQPConsumer.new(self, bunny, q)
           @consumer.on_delivery() do |delivery_info, metadata, payload|
@@ -24,6 +24,29 @@ module Batsir
 
       def handle_delivery(payload)
         start_filter_chain(payload)
+      end
+
+      def bind_queue_to_exchange(q, x)
+        max_retries = 3
+        result = nil
+        error = nil
+        n = 0
+
+        while !result && n < max_retries
+          begin
+            result = q.bind( x, :routing_key => queue)
+          rescue ClientTimeout => error
+            log.warn "Caught ClientTimeout while trying to bind queue to exchange, retrying (##{n+1})"
+          ensure
+            n += 1
+          end
+        end
+
+        if !result && n >= max_retries
+          raise error
+        end
+
+        result
       end
 
       def process_message_error(message, error)
