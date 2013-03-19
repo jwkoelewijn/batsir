5fd08257d76bd721bdd5c82ae38237fb98b40516
Bram de Vries <dev@bramdevries.net>
2013-03-19 10:29:28 +0100

    AMQPNotifier instance has a dedicated bunny

--- lib/batsir/notifiers/amqp_notifier.rb
+++ lib/batsir/notifiers/amqp_notifier.rb
@@ -10,19 +10,12 @@ module Batsir
       def initialize(options = {}, error_strategy = Batsir::Strategies::RetryStrategy)
         super(options)
         @error_strategy = error_strategy.new(self)
+        @bunny = Bunny.new(bunny_options).start
+        @x = @bunny.exchange( exchange )
       end
 
       def execute(message)
-        begin
-          bunny_pool.with do |bunny|
-            x = bunny.exchange(exchange)
-            x.publish(message, :routing_key => queue)
-          end
-        rescue Bunny::ProtocolError => e
-          handle_error(message, e)
-        rescue Bunny::ForcedConnectionCloseError => e
-          handle_error(message, e)
-        end
+        @x.publish(message, :routing_key => queue)
       end
 
       def handle_error(message, error)
