ad425ec16d5ec797064201d0e73824b39a7ab0e3
Bram de Vries <dev@bramdevries.net>
2013-03-19 08:54:37 +0100

    AMQPAcceptor instance has a dedicated bunny

--- lib/batsir/acceptors/amqp_acceptor.rb
+++ lib/batsir/acceptors/amqp_acceptor.rb
@@ -8,45 +8,25 @@ module Batsir
 
       attr_reader :consumer
 
-      def start
-        bunny_pool.with do |bunny|
-          q = bunny.queue( queue )
-          x = bunny.exchange( exchange )
-          bind_queue_to_exchange(q,x)
+      def initialize(options={})
+        super
+        @bunny = Bunny.new(bunny_options).start
+        @q = @bunny.queue( queue )
+        @x = @bunny.exchange( exchange )
+        @q.bind( @x, :routing_key => queue)
 
-          @consumer = Batsir::AMQPConsumer.new(self, bunny, q)
-          @consumer.on_delivery() do |delivery_info, metadata, payload|
-            handle_delivery(payload)
-          end
-          q.subscribe_with(@consumer)
+        @consumer = Batsir::AMQPConsumer.new(self, @bunny, @q)
+        @consumer.on_delivery() do |delivery_info, metadata, payload|
+          handle_delivery(payload)
         end
+      end
+
+      def start
+        @q.subscribe_with(@consumer)
       end
 
       def handle_delivery(payload)
         start_filter_chain(payload)
-      end
-
-      def bind_queue_to_exchange(q, x)
-        max_retries = 3
-        result = nil
-        error = nil
-        n = 0
-
-        while !result && n < max_retries
-          begin
-            result = q.bind( x, :routing_key => queue)
-          rescue ClientTimeout => error
-            log.warn "Caught ClientTimeout while trying to bind queue to exchange, retrying (##{n+1})"
-          ensure
-            n += 1
-          end
-        end
-
-        if !result && n >= max_retries
-          raise error
-        end
-
-        result
       end
 
       def process_message_error(message, error)
