ad1f86945dd4ba4e73ca27647e5130a2fa7e980b
Bram de Vries <dev@bramdevries.net>
2013-03-27 11:46:11 +0100

    Explicitly set AMQP heartbeat

--- spec/batsir/amqp_spec.rb
+++ spec/batsir/amqp_spec.rb
@@ -32,13 +32,23 @@ describe Batsir::AMQP do
     it 'has a exchange' do
       @test.exchange.should == 'amq.direct'
     end
+
+    it 'is undead' do
+      @test.heartbeat.should == 0
+    end
   end
 
   context 'with respect to setting variables' do
-    it 'can change their values' do
+    it 'can change host' do
       localhost = '127.0.0.1'
       @test.host = localhost
       @test.host.should == localhost
+    end
+
+    it 'can change heartbeat' do
+      hb = 512
+      @test.heartbeat = hb
+      @test.heartbeat.should == hb
     end
   end
 end
