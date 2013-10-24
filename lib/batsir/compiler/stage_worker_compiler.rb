60bfa77e02d43e3afb3f7e412a153af79ba87d1d
Bram de Vries <dev@blaet.net>
2013-10-24 16:58:27 +0200

    Second stage

--- lib/batsir/compiler/stage_worker_compiler.rb
+++ lib/batsir/compiler/stage_worker_compiler.rb
@@ -9,30 +9,36 @@ module Batsir
 
       def compile
         klazz_name = "#{stage.name.capitalize.gsub(' ','')}Worker"
-        worker_class_code(klazz_name)
+        compile_worker_class(klazz_name)
       end
 
-      def worker_class_code(klazz_name)
-        code = <<-EOF
-          class #{klazz_name}
-        EOF
-
-        add_some_methods(code)
-
-        add_filter_queue(code)
-
-        code << <<-EOF
-            include Sidekiq::Worker
-            include Batsir::StageWorker
+      def compile_worker_class(klazz_name)
+        worker_class(klazz_name) do |code|
+          stage.filter_declarations.each do |filter_declaration|
+            code << <<-EOF
+                @filter_queue.add #{filter_declaration.filter.to_s}.new(#{filter_declaration.options.to_s})
+            EOF
           end
 
-          #{klazz_name}.sidekiq_options(:queue => Batsir::Config.sidekiq_queue)
-          #{klazz_name}
-        EOF
+          stage.conditional_notifiers.each do |conditional_notifier_declaration|
+            conditional_notifier_declaration.compile(code, self)
+          end
+
+          stage.notifiers.each do |notifier, options_set|
+            options_set.each do |options|
+              code << <<-EOF
+                notifier = #{notifier.to_s}.new(#{options.to_s})
+              EOF
+              self.add_transformers_to_notifier("notifier", code)
+              self.add_notifier("notifier", code)
+            end
+          end
+        end
       end
 
-      def add_some_methods(code)
-        code << <<-EOF
+      def worker_class(klazz_name, &block)
+        code = <<-EOF
+          class #{klazz_name}
             def self.stage_name
               "#{stage.name}"
             end
@@ -44,52 +50,23 @@ module Batsir
             def self.filter_queue
               @filter_queue
             end
-        EOF
-      end
 
-      def add_filter_queue(code)
-        code << <<-EOF
             def self.initialize_filter_queue
               @filter_queue = Batsir::FilterQueue.new
         EOF
 
-        add_filter_declarations(code)
-
-        add_conditional_notifiers(code)
-
-        add_notifiers(code)
+        yield code
 
         code << <<-EOF
             end
-        EOF
-      end
 
-      def add_filter_declarations(code)
-        stage.filter_declarations.each do |filter_declaration|
-          code << <<-EOF
-              @filter_queue.add #{filter_declaration.filter.to_s}.new(#{filter_declaration.options.to_s})
-          EOF
-        end
-      end
-
-      def add_conditional_notifiers(code)
-        stage.conditional_notifiers.each do |conditional_notifier_declaration|
-          conditional_notifier_declaration.compile(code, self)
-        end
-      end
-
-      def add_notifiers(code)
-        stage.notifiers.each do |notifier, options_set|
-          options_set.each do |options|
-            code << <<-EOF
-              notifier = #{notifier.to_s}.new(#{options.to_s})
-            EOF
-
-            self.add_transformers_to_notifier("notifier", code)
-
-            self.add_notifier("notifier", code)
+            include Sidekiq::Worker
+            include Batsir::StageWorker
           end
-        end
+
+          #{klazz_name}.sidekiq_options(:queue => Batsir::Config.sidekiq_queue)
+          #{klazz_name}
+        EOF
       end
 
       def add_transformers_to_notifier(notifier_name, code)
